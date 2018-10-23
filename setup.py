ee75f9a8882585413c31765efc1edc2344117048
lsawakuchi <lidia.sawakuchi@gmail.com>
2018-10-23 08:41:46 -0300

    package files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+version = '1.0.0'
+
+setup(name='xray',
+      version=version,
+      description="Data library developed change how we understand Education",
+      url="https://github.com/lsawakuchi/Desafio",
+      author="Lidia Sawakuchi",
+      author_email="lidia.sawakuchi@gmail.com",
+      license="MIT",
+      keywords="gamma challenge",
+      packages=["E-xray"],
+      zip_safe=False)
